d5b66c16c8db3dbcf011c8a9fe4a46cbd057744b
Laxmikant <laxmikantmaity1944@gmail.com>
2026-03-15 10:54:28 +0530

    Update debian-rice.sh

--- scripts/debian/debian-rice.sh
+++ scripts/debian/debian-rice.sh
@@ -54,7 +54,7 @@ banner "Updating the System"
 if ask "Shall we proceed with upgrading the system and Installing all Dependecies sir ?? [Y/n]"; then
     sudo apt update -y
     sudo apt upgrade
-    sudo apt install -y fish fastfetch curl libgnome-menu-3-0 gir1.2-gmenu-3.0 ptyxis pipx wlogout gnome-extensions-app gnome-shell-extension-caffeine gnome-shell-extension-gsconnect gnome-shell-extension-user-theme 
+    sudo apt install -y fish fastfetch curl libgnome-menu-3-0 gir1.2-gmenu-3.0 ptyxis pipx wlogout gnome-extensions-app gnome-shell-extension-caffeine gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-launch-new-instance gnome-shell-extensions-extra
 fi
 
 # =============================================================================
